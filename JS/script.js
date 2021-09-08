d3239d2d882a5079990f686f8a07882294e95960
fb0801 <fbhatti0801@gmail.com>
2021-09-08 19:36:14 +0100

    phase 5

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,22 @@
+const startButton = document.getElementById('start-btn'); //use const as this btn wont change
+const questionContainerElement = document.getElementById('question-container');
+const shuffleQuestions, currentQuestionIndex;
+
+const questions = [
+    {
+
+}
+];
+
+
+startButton.addEventListener('click', startGame)
+
 function startGame(){
     //to start the game
-
+console.log('started');
+startButton.classList.add('hide');
+questionContainerElement.classList.remove('hide');
+setNextQuestion()
 
 
 
@@ -12,6 +28,12 @@ function setNextQuestion(){
 
 
 
+
+
+}
+
+function selectAnswer(){
+//select the answer
 
 
 }
